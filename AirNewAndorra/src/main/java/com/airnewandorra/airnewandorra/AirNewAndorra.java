f09e658dfa7b80368cf232b3612fb2afccce9a85
David Salazar <davids1_4@hotmail.com>
2023-06-07 21:49:05 +0200

    Clean

--- AirNewAndorra/src/main/java/com/airnewandorra/airnewandorra/AirNewAndorra.java
+++ AirNewAndorra/src/main/java/com/airnewandorra/airnewandorra/AirNewAndorra.java
@@ -1,13 +1,5 @@
 package com.airnewandorra.airnewandorra;
 
-import com.airnewandorra.models.Destino;
-import com.airnewandorra.models.Origen;
-import com.airnewandorra.models.TipoAvion;
-import com.airnewandorra.models.Vuelo;
-import com.airnewandorra.data.Datos;
-
-import java.util.ArrayList;
-
 /**
  * @author minak
  */
