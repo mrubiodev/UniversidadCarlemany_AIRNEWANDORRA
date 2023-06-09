248b8c9eeccbadc4a3062ed7766d8399cecd3ba2
mrubio <mario.rubio_avila@siemens.com>
2023-06-09 15:17:45 +0200

    Primer acercamiento del menu

--- AirNewAndorra/src/main/java/com/airnewandorra/airnewandorra/AirNewAndorra.java
+++ AirNewAndorra/src/main/java/com/airnewandorra/airnewandorra/AirNewAndorra.java
@@ -1,11 +1,129 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
 package com.airnewandorra.airnewandorra;
 
+import utils.ToolsAndMenu;
+import static utils.ToolsAndMenu.menu;
+import static utils.ToolsAndMenu.title;
+
+
 /**
+ *
  * @author minak
  */
 public class AirNewAndorra {
+    
+    public static Datos datosPrograma;
+    
+    static void menuPrincipal(){
 
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+        datosPrograma = new Datos();
+
+        title ("AirNewAndorra");
+        String[] menuOpciones = {"1.Cliente", "2.Gestor", "0.Salir"};
+        while (true){  //Bucle infinito
+            int option = menu(menuOpciones);
+            switch (option){
+                case 0://SALIDA APP
+                    System.out.println("Volviendo al menu principal");
+                    //ToolsAndMenu.sleepThread(0);//Pausa por tiempo
+                    return;//Me voy de la funcion
+                    //break;
+                case 1:
+                    menuCliente();
+                    ToolsAndMenu.pausa();
+                    break;
+                case 2:                
+                    menuGestor();
+                    ToolsAndMenu.pausa();
+                    break;
+            }
+        }
+    }
+    
+    static void menuCliente(){
+        String[] titulos = {"AirNewAndorra", "1.Cliente"};
+        String[] menuOpciones = {"1.Crear Pasajero", "2.Seleccionar Pasajero", "0.Volver"};
+        title (titulos);
+        int option = menu(menuOpciones);
+    }
+    
+    static void menuGestor(){
+        String[] titulos = {"AirNewAndorra", "2.Gestor"};
+        String[] menuOpciones = {"1.Vuelos", "2.Clases", "3.Destinos", "4.Clases Vuelos", "0.Volver"};
+        title (titulos);
+
+        while (true){  //Bucle infinito
+            int option = menu(menuOpciones);
+            switch (option){
+                case 0://SALIDA APP
+                    System.out.println("Volviendo al menu principal");
+                    //ToolsAndMenu.sleepThread(0);//Pausa por tiempo
+                    return;//Me voy de la funcion
+                    //break;
+                case 1:
+                    menuVuelos();
+                    ToolsAndMenu.pausa();
+                    break;
+                case 2:                
+                    menuClases();
+                    ToolsAndMenu.pausa();
+                    break;
+                case 3:                
+                    menuDestinos();
+                    ToolsAndMenu.pausa();
+                    break;
+                case 4:                
+                    menuClasesDeVuelos();
+                    ToolsAndMenu.pausa();
+                    break;
+            }
+        }
+    }
+
+    static void menuClases(){
+        String[] titulos = {"AirNewAndorra", "2.Gestor", "2.Clases"};
+        String[] menuOpciones = {"1.Mostrar Clases", "0.Volver"};
+        title (titulos);
+        while (true){  //Bucle infinito
+            int option = menu(menuOpciones);
+            switch (option){
+                case 0://SALIDA APP
+                    System.out.println("Volviendo al menu principal");
+                    //ToolsAndMenu.sleepThread(0);//Pausa por tiempo
+                    return;//Me voy de la funcion
+                    //break;
+                case 1:
+                    datosPrograma.MostrarClases();
+                    ToolsAndMenu.pausa();
+                    break;
+            }
+        }
+    }    
+
+    static void menuDestinos(){
+        String[] titulos = {"AirNewAndorra", "2.Gestor", "3.Destinos"};
+        String[] menuOpciones = {"1.Mostrar Clases", "0.Volver"};
+        title (titulos);
+        while (true){  //Bucle infinito
+            int option = menu(menuOpciones);
+            switch (option){
+                case 0://SALIDA APP
+                    System.out.println("Volviendo al menu principal");
+                    //ToolsAndMenu.sleepThread(0);//Pausa por tiempo
+                    return;//Me voy de la funcion
+                    //break;
+                case 1:
+                    datosPrograma.MostrarDestinos();
+                    ToolsAndMenu.pausa();
+                    break;
+            }
+        }
+    }    
+    
+    public static void main(String[] args) {
+        menuPrincipal();
+    }
 }
---
+++ AirNewAndorra/src/main/java/utils/ColorConsole.java
@@ -0,0 +1,21 @@
+
+package utils;
+/**
+ * @author Mario Rubio Avila
+ * @version 0.1
+ * @since  2019
+ * Son las constantes para colorear el texto y ayuda en la visibilidad ya que
+ * en la tarea 6.1 de programacion en practicas de termina se empezo a encontrar problemas para tener una clara visibilidad
+ * de los mensajes.
+ */
+public class ColorConsole {
+    static public final String BLACK="\033[30m"; 
+    static public final String RED="\033[31m"; 
+    static public final String GREEN="\033[32m"; 
+    static public final String YELLOW="\033[33m"; 
+    static public final String BLUE="\033[34m"; 
+    static public final String PURPLE="\033[35m"; 
+    static public final String CYAN="\033[36m"; 
+    static public final String WHITE="\033[37m";
+    static public final String RESET="\u001B[0m";
+}
